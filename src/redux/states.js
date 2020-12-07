5a14fd1676177843389bcaca7c52130c32ac7b06
Oussemax1 <amaroussema@gmail.com>
2020-12-07 13:49:38 +0100

    redux

---
+++ src/redux/states.js
@@ -0,0 +1,20 @@
+export let todos = [
+    {
+      id: 1,
+      name: "One",
+      description: "Waking up ",
+      IsDone: true,
+    },
+    {
+      id: 2,
+      name: "Two",
+      description: "Breakfast",
+      IsDone: true,
+    },
+    {
+      id: 3,
+      name: "Three",
+      description: "Go to the work",
+      IsDone: true,
+    },
+  ];
